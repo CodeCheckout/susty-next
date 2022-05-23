011c275a7d85e217b620e19b77c1556c9822b3f7
Flashminat0 <Flashminat0@users.noreply.github.com>
2022-05-23 06:20:36 +0000

    Prettified Code!

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,11 @@
-import React from 'react';
+import React from 'react'
 
 const Index = () => {
     return (
         <div className={`bg-red-500 text-blue-400 text-lg font-bold`}>
             hello world
         </div>
-    );
-};
+    )
+}
 
-export default Index;
+export default Index
