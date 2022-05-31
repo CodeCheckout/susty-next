9386cf694e63b251335746c25cb791c959e4057b
Flashminat0 <Flashminat0@users.noreply.github.com>
2022-05-31 07:33:17 +0000

    Prettified Code!

--- components/Lists/Products/PopularItems.jsx
+++ components/Lists/Products/PopularItems.jsx
@@ -1,36 +1,43 @@
-import React, {useEffect, useState} from 'react';
-import PopularItemLayout from "../../layouts/ItemRows/PopularItemLayout";
-import axios from "axios";
-import PopularItemCard from "../../Cards/Item/PopularItemCard";
-
+import React, {useEffect, useState} from 'react'
+import PopularItemLayout from '../../layouts/ItemRows/PopularItemLayout'
+import axios from 'axios'
+import PopularItemCard from '../../Cards/Item/PopularItemCard'
 
 const PopularItems = () => {
-    const [items, setItems] = useState([]);
+    const [items, setItems] = useState([])
 
     useEffect(() => {
-        axios.get('/api/product/fetch-products',{
-            params: {
-                productLimit : 10
-            }
-        }).then(res => {
-            setItems(res.data.products);
-        })
-    }, []);
+        axios
+            .get('/api/product/fetch-products', {
+                params: {
+                    productLimit: 10,
+                },
+            })
+            .then((res) => {
+                setItems(res.data.products)
+            })
+    }, [])
 
     return (
         <PopularItemLayout>
-            {items.length > 0 && <>
-                {items.map((item) => (
-                    <div key={item.id}>
-                        <PopularItemCard src={item.images[0]?.url} alt={item.images[0]?.name} price={item.price} size={item.size}
-                                         brand={item.brand} favCount={item.favouriteCount}/>
-                    </div>
-                ))}
-            </>}
-
+            {items.length > 0 && (
+                <>
+                    {items.map((item) => (
+                        <div key={item.id}>
+                            <PopularItemCard
+                                src={item.images[0]?.url}
+                                alt={item.images[0]?.name}
+                                price={item.price}
+                                size={item.size}
+                                brand={item.brand}
+                                favCount={item.favouriteCount}
+                            />
+                        </div>
+                    ))}
+                </>
+            )}
         </PopularItemLayout>
-    );
-};
+    )
+}
 
-export default PopularItems;
-
+export default PopularItems
