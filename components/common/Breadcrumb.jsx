ab732042021815e8c48424287ec0d86ad1205823
Flashminat0 <Flashminat0@users.noreply.github.com>
2022-06-06 19:02:06 +0000

    Prettified Code!

--- components/common/Breadcrumb.jsx
+++ components/common/Breadcrumb.jsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import {HiHome , HiChevronRight} from "react-icons/hi";
+import React from 'react'
+import {HiHome, HiChevronRight} from 'react-icons/hi'
 
 const pages = [
     {name: 'Projects', href: '#', current: false},
@@ -12,8 +12,14 @@ const Breadcrumb = ({pagePathArray}) => {
             <ol role="list" className="flex items-center space-x-4">
                 <li>
                     <div>
-                        <a href="#" className="text-gray-400 hover:text-gray-500">
-                            <HiHome className="flex-shrink-0 h-5 w-5" aria-hidden="true"/>
+                        <a
+                            href="#"
+                            className="text-gray-400 hover:text-gray-500"
+                        >
+                            <HiHome
+                                className="flex-shrink-0 h-5 w-5"
+                                aria-hidden="true"
+                            />
                             <span className="sr-only">Home</span>
                         </a>
                     </div>
@@ -21,9 +27,16 @@ const Breadcrumb = ({pagePathArray}) => {
                 {pagePathArray.map((page) => (
                     <li key={page}>
                         <div className="flex items-center">
-                            <HiChevronRight className="flex-shrink-0 h-5 w-5 text-gray-400" aria-hidden="true"/>
+                            <HiChevronRight
+                                className="flex-shrink-0 h-5 w-5 text-gray-400"
+                                aria-hidden="true"
+                            />
                             <a
-                                href={page.split(' ').join('-').trim().toLowerCase()}
+                                href={page
+                                    .split(' ')
+                                    .join('-')
+                                    .trim()
+                                    .toLowerCase()}
                                 className="ml-4 text-sm font-medium text-gray-500 hover:text-gray-700"
                                 aria-current={page.current ? 'page' : undefined}
                             >
@@ -34,7 +47,7 @@ const Breadcrumb = ({pagePathArray}) => {
                 ))}
             </ol>
         </nav>
-    );
-};
+    )
+}
 
-export default Breadcrumb;
+export default Breadcrumb
--- pages/items/view.js
+++ pages/items/view.js
@@ -1,15 +1,15 @@
-import React, {useEffect, useState} from 'react';
-import {useRouter} from "next/router";
-import axios from "axios";
-import ProductCarousel from "../../components/Cards/Carousels/ProductCarousel";
-import SingleProductDetails from "../../components/details/SingleProductDetails";
-import Breadcrumb from "../../components/common/Breadcrumb";
+import React, {useEffect, useState} from 'react'
+import {useRouter} from 'next/router'
+import axios from 'axios'
+import ProductCarousel from '../../components/Cards/Carousels/ProductCarousel'
+import SingleProductDetails from '../../components/details/SingleProductDetails'
+import Breadcrumb from '../../components/common/Breadcrumb'
 
 const View = () => {
     const router = useRouter()
     const {itemId} = router.query
 
-    const [productDetails, setProductDetails] = useState(null);
+    const [productDetails, setProductDetails] = useState(null)
 
     useEffect(() => {
         const fetchProducts = async () => {
@@ -27,24 +27,35 @@ const View = () => {
                 })
         }
         fetchProducts()
-
     }, [router.query])
 
-    return (<div>
-        {productDetails && <>
-            <div className="bg-white">
-                <div className="max-w-2xl mx-auto py-8 px-4 sm:py-12 sm:px-6 lg:max-w-7xl lg:px-8">
-                    <Breadcrumb pagePathArray={[productDetails.mainCategory , productDetails.subCategory1 , productDetails.subCategory2]}/>
-                    <div className="lg:grid lg:grid-cols-2 lg:gap-x-8 lg:items-start">
-                        <ProductCarousel productImageArray={productDetails.images}/>
-                        <SingleProductDetails productDetails={productDetails}/>
+    return (
+        <div>
+            {productDetails && (
+                <>
+                    <div className="bg-white">
+                        <div className="max-w-2xl mx-auto py-8 px-4 sm:py-12 sm:px-6 lg:max-w-7xl lg:px-8">
+                            <Breadcrumb
+                                pagePathArray={[
+                                    productDetails.mainCategory,
+                                    productDetails.subCategory1,
+                                    productDetails.subCategory2,
+                                ]}
+                            />
+                            <div className="lg:grid lg:grid-cols-2 lg:gap-x-8 lg:items-start">
+                                <ProductCarousel
+                                    productImageArray={productDetails.images}
+                                />
+                                <SingleProductDetails
+                                    productDetails={productDetails}
+                                />
+                            </div>
+                        </div>
                     </div>
-                </div>
-            </div>
+                </>
+            )}
+        </div>
+    )
+}
 
-        </>}
-
-    </div>);
-};
-
-export default View;
+export default View
