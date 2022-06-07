89f2d113bbd8e4047164e49b09403169d25dab67
Flashminat0 <Flashminat0@users.noreply.github.com>
2022-06-07 10:21:57 +0000

    Prettified Code!

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import CTA from '../components/home/CTA'
 import BasicPageWrapper from '../components/layouts/BasicPageWrapper'
 import PopularItems from '../components/Lists/Products/PopularItems'
-import Header from "../components/common/Header";
+import Header from '../components/common/Header'
 
 const Index = () => {
     return (
