740e513d89611055ad47cfe27ceae419391d3347
Flashminat0 <Flashminat0@users.noreply.github.com>
2022-06-12 11:55:25 +0000

    Prettified Code!

--- pages/index.js
+++ pages/index.js
@@ -2,14 +2,14 @@ import React from 'react'
 import CTA from '../components/home/CTA'
 import BasicPageWrapper from '../components/layouts/BasicPageWrapper'
 import PopularItems from '../components/Lists/Products/PopularItems'
-import ShopByBrand from "../components/home/ShopByBrand";
+import ShopByBrand from '../components/home/ShopByBrand'
 
 const Index = () => {
     return (
         <BasicPageWrapper>
-            <CTA/>
-            <PopularItems/>
-            <ShopByBrand/>
+            <CTA />
+            <PopularItems />
+            <ShopByBrand />
         </BasicPageWrapper>
     )
 }
