b4d36b21976471ff41a676d35c579331870fcc88
Flashminat0 <wyasaslowe@gmail.com>
2022-06-12 18:08:09 +0530

    SuggestedSearches added

--- components/home/CTA.jsx
+++ components/home/CTA.jsx
@@ -1,86 +1,81 @@
-import React from 'react'
-import {useRouter} from 'next/router'
+import React, {useState} from 'react';
+import {useRouter} from "next/router";
+import {AnimatePresence, motion} from "framer-motion";
+
+const slidesStaticData = [{
+    id: 1,
+    title: 'Women',
+    src: 'https://firebasestorage.googleapis.com/v0/b/susty-next.appspot.com/o/Slide1.png?alt=media&token=54dc1d99-b1f2-4059-a261-a8f2b6be3365',
+    alt: "Women with Cloths Image"
+}, {
+    id: 2,
+    title: 'Men',
+    src: 'https://firebasestorage.googleapis.com/v0/b/susty-next.appspot.com/o/Slide2.png?alt=media&token=c23cb27e-4493-4084-90b4-740fb58cb2f5',
+    alt: "Mens with Cloths Image"
+}, {
+    id: 3,
+    title: 'Kids',
+    src: 'https://firebasestorage.googleapis.com/v0/b/susty-next.appspot.com/o/Slide3.png?alt=media&token=11651c89-7afd-4107-934e-74f8e62ea817',
+    alt: "Kids with Cloths Image"
+}]
 
 const CTA = () => {
-    const router = useRouter()
+    const [slides, setSlides] = useState(slidesStaticData);
 
-    const slides = {
-        women: 'https://firebasestorage.googleapis.com/v0/b/susty-next.appspot.com/o/Slide1.png?alt=media&token=54dc1d99-b1f2-4059-a261-a8f2b6be3365',
-        men: 'https://firebasestorage.googleapis.com/v0/b/susty-next.appspot.com/o/Slide2.png?alt=media&token=c23cb27e-4493-4084-90b4-740fb58cb2f5',
-        kids: 'https://firebasestorage.googleapis.com/v0/b/susty-next.appspot.com/o/Slide3.png?alt=media&token=11651c89-7afd-4107-934e-74f8e62ea817',
-    }
+    const router = useRouter();
 
     return (
         <>
-            <div className={'hidden lg:block px-20'}>
+            <div className={'hidden lg:block pt-2'}>
                 <div className={'grid grid-cols-4 gap-2'}>
                     <div className={'col-span-3'}>
-                        <div className="flex flex-col gap-3 w-full">
-                            <div
-                                className="relative w-full group bg-gray-800 duration-300 cursor-pointer"
-                                onClick={async () => await router.push(`/`)}
-                            >
-                                <p className="absolute bottom-3 left-16 text-white text-2xl transition ease-in-out delay-150 group-hover:underline group-hover:-translate-y-2">
-                                    Women
-                                </p>
-                                <img
-                                    src={slides.women}
-                                    className="w-full h-full my-auto object-cover group-hover:opacity-40 transition-opacity"
-                                    alt="Logo"
-                                />
+                        <AnimatePresence>
+                            <div className="flex flex-col gap-3 w-full">
+                                {slides.map((slide) => (
+                                    <motion.div
+                                        key={slide.id}
+                                        whileHover={{
+                                            scale: 1.01,
+                                        }}
+                                        className="relative w-full group bg-gray-800 duration-300 cursor-pointer"
+                                        onClick={async () => await router.push(`/`)}
+                                    >
+                                        <p className="absolute bottom-3 left-16 text-white text-2xl transition ease-in-out delay-150 group-hover:underline group-hover:-translate-y-2">
+                                            {slide.title}
+                                        </p>
+                                        <img
+                                            src={slide.src}
+                                            className="w-full h-full my-auto object-cover group-hover:opacity-40 transition-opacity"
+                                            alt={slide.alt}
+                                        />
+                                    </motion.div>
+                                ))}
                             </div>
-                            <div
-                                className="relative w-full group bg-gray-800 duration-300 cursor-pointer"
-                                onClick={async () => await router.push(`/`)}
-                            >
-                                <p className="absolute bottom-3 left-16 text-white text-2xl transition ease-in-out delay-150 group-hover:underline group-hover:-translate-y-2">
-                                    Men
-                                </p>
-                                <img
-                                    src={slides.men}
-                                    className="w-full h-full my-auto object-cover group-hover:opacity-40 transition-opacity "
-                                    alt="Logo"
-                                />
-                            </div>
-                            <div
-                                className="relative w-full group bg-gray-800 duration-300 cursor-pointer"
-                                onClick={async () => await router.push(`/`)}
-                            >
-                                <p className="absolute bottom-3 left-16 text-white text-2xl transition ease-in-out delay-150 group-hover:underline group-hover:-translate-y-2">
-                                    Kids
-                                </p>
-                                <img
-                                    src={slides.kids}
-                                    className="w-full h-full my-auto object-cover group-hover:opacity-40 transition-opacity"
-                                    alt="Logo"
-                                />
-                            </div>
-                        </div>
+                        </AnimatePresence>
                     </div>
                     <div className={'col-span-1'}>
                         <div className="bg-white h-full w-full border flex flex-col border-red-300 p-5">
-                            <p className="text-5xl font-bold lg:pt-32 mx-auto">
+                            <p className="text-4xl font-bold pt-16 mx-auto leading-relaxed">
                                 Recycle your
                             </p>
-                            <p className="text-5xl font-bold pt-6 mx-auto">
+                            <p className="text-4xl font-bold pt-6 mx-auto">
                                 Clothes for
                             </p>
-                            <p className="text-5xl font-bold pt-6 mx-auto">
+                            <p className="text-4xl font-bold py-6 mx-auto">
                                 money?
                             </p>
-                            <button
+                            <motion.button
                                 onClick={async () =>
                                     await router.push(`/items/add`)
                                 }
-                                className={
-                                    'px-4 py-3 mt-auto w-full text-xl bg-red-400 hover:bg-red-50 focus:bg-white text-white hover:text-red-400 focus:text-black border border-red-400 hover:border-red-400 focus:border-black'
-                                }
-                            >
+                                whileHover={{scale: 1.02}}
+                                whileTap={{scale: 0.98}}
+                                className={`inline-flex justify-center items-center capitalize px-4 py-1.5 border border-gray-300 shadow-sm text-base font-medium rounded-md text-white bg-susty hover:bg-white hover:text-susty hover:border-susty focus:text-red-400 focus:border-susty focus:bg-red-50`}>
                                 Sell Now
-                            </button>
+                            </motion.button>
                             <div
                                 onClick={async () => await router.push(`/`)}
-                                className="text-red-400 underline text-lg mt-10 hover:text-black no-underline"
+                                className="text-red-400 underline text-sm pt-10 hover:text-black no-underline"
                             >
                                 Learn How it works
                             </div>
@@ -88,11 +83,11 @@ const CTA = () => {
                     </div>
                 </div>
             </div>
-            <div className={'block lg:hidden mt-6'}>
+            <div className={'block lg:hidden px-2 pt-2'}>
                 <div className={'grid grid-rows-3 gap-8'}>
                     <div className={'row-span-2'}>
                         <div className="bg-white h-full w-full border flex flex-col border-red-300 p-5">
-                            <p className="text-5xl font-bold pt-16 mx-auto">
+                            <p className="text-5xl font-bold pt-16 mx-auto leading-relaxed">
                                 Recycle your
                             </p>
                             <p className="text-5xl font-bold pt-6 mx-auto">
@@ -101,69 +96,52 @@ const CTA = () => {
                             <p className="text-5xl font-bold py-6 mx-auto">
                                 money?
                             </p>
-                            <button
-                                onClick={async () => await router.push(`/`)}
-                                className={
-                                    'px-4 py-3 mt-auto w-full text-xl bg-red-400 hover:bg-red-50 focus:bg-white text-white hover:text-red-400 focus:text-black border border-red-400 hover:border-red-400 focus:border-black'
+                            <motion.button
+                                onClick={async () =>
+                                    await router.push(`/items/add`)
                                 }
-                            >
+                                whileHover={{scale: 1.02}}
+                                whileTap={{scale: 0.98}}
+                                className={`inline-flex justify-center items-center capitalize px-4 py-2 border border-gray-300 shadow-sm text-base font-medium rounded-md text-white bg-susty hover:bg-white hover:text-susty hover:border-susty focus:text-red-400 focus:border-susty focus:bg-red-50`}>
                                 Sell Now
-                            </button>
+                            </motion.button>
                             <div
                                 onClick={async () => await router.push(`/`)}
-                                className="text-red-400 underline text-lg mt-10 hover:text-black no-underline"
+                                className="text-red-400 underline text-sm mt-10 hover:text-black no-underline"
                             >
                                 Learn How it works
                             </div>
                         </div>
                     </div>
                     <div className={'row-span-1'}>
-                        <div className="flex flex-row gap-3 h-full w-full">
-                            <div
-                                className="relative h-full w-full group bg-gray-800 duration-300 cursor-pointer"
-                                onClick={async () => await router.push(`/`)}
-                            >
-                                <p className="absolute bottom-3 left-16 text-white text-2xl transition ease-in-out delay-150 group-hover:underline group-hover:-translate-y-2">
-                                    Women
-                                </p>
-                                <img
-                                    src={slides.women}
-                                    className="w-full h-full object-cover object-left group-hover:opacity-40 transition-opacity"
-                                    alt="Logo"
-                                />
+                        <AnimatePresence>
+                            <div className="flex flex-row gap-3 h-[12rem] w-full">
+                                {slides.map((slide) => (
+                                    <motion.div
+                                        key={slide.id}
+                                        whileHover={{
+                                            scale: 1.01,
+                                        }}
+                                        className="relative h-full w-full group bg-gray-800 duration-300 cursor-pointer"
+                                        onClick={async () => await router.push(`/`)}
+                                    >
+                                        <p className="absolute bottom-3 left-16 text-white text-xl sm:text-2xl transition ease-in-out delay-150 group-hover:underline group-hover:-translate-y-2">
+                                            {slide.title}
+                                        </p>
+                                        <img
+                                            src={slide.src}
+                                            className="w-full h-full object-cover object-center group-hover:opacity-40 transition-opacity"
+                                            alt={slide.alt}
+                                        />
+                                    </motion.div>
+                                ))}
                             </div>
-                            <div
-                                className="relative h-full w-full group bg-gray-800 duration-300 cursor-pointer"
-                                onClick={async () => await router.push(`/`)}
-                            >
-                                <p className="absolute bottom-3 left-16 text-white text-2xl transition ease-in-out delay-150 group-hover:underline group-hover:-translate-y-2">
-                                    Men
-                                </p>
-                                <img
-                                    src={slides.men}
-                                    className="w-full h-full object-cover group-hover:opacity-40 transition-opacity"
-                                    alt="Logo"
-                                />
-                            </div>
-                            <div
-                                className="relative h-full w-full group bg-gray-800 duration-300 cursor-pointer"
-                                onClick={async () => await router.push(`/`)}
-                            >
-                                <p className="absolute bottom-3 left-16 text-white text-2xl transition ease-in-out delay-150 group-hover:underline group-hover:-translate-y-2">
-                                    Kids
-                                </p>
-                                <img
-                                    src={slides.kids}
-                                    className="w-full h-full object-cover group-hover:opacity-40 transition-opacity"
-                                    alt="Logo"
-                                />
-                            </div>
-                        </div>
+                        </AnimatePresence>
                     </div>
                 </div>
             </div>
         </>
-    )
-}
+    );
+};
 
-export default CTA
+export default CTA;
--- components/layouts/BasicPageWrapper.jsx
+++ components/layouts/BasicPageWrapper.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
 
 const BasicPageWrapper = ({children}) => {
-    return <div className={`px-2 py-3 sm:px-4 lg:px-14`}>{children}</div>
+    return (
+        <div className={`px-2 py-3 sm:px-4 lg:px-14`}>
+            {children}
+        </div>
+    );
 }
 
 export default BasicPageWrapper
