101d6525beb2b8fb2769398dc9a634978ff305a2
Flashminat0 <Flashminat0@users.noreply.github.com>
2022-06-15 04:14:55 +0000

    Prettified Code!

--- components/details/RatesInClosetSpotlight.jsx
+++ components/details/RatesInClosetSpotlight.jsx
@@ -1,20 +1,34 @@
-import React from 'react';
-import {HiStar} from "react-icons/all";
+import React from 'react'
+import {HiStar} from 'react-icons/all'
 
 const RatesInClosetSpotlight = ({person, personD, id}) => {
     return (
         <div className={'flex flex-row items-center'} key={id}>
-            {Array(Math.ceil(parseFloat(personD.rating))).fill(0).map((x, index) => {
-                return <HiStar key={index} className={'w-5 h-5 text-amber-400'}/>
-            })}
-            {Array(Math.floor(5 - parseFloat(personD.rating))).fill(0).map((x, index) => {
-                return <HiStar key={index} className={'w-5 h-5 text-gray-300'}/>
-            })}
+            {Array(Math.ceil(parseFloat(personD.rating)))
+                .fill(0)
+                .map((x, index) => {
+                    return (
+                        <HiStar
+                            key={index}
+                            className={'w-5 h-5 text-amber-400'}
+                        />
+                    )
+                })}
+            {Array(Math.floor(5 - parseFloat(personD.rating)))
+                .fill(0)
+                .map((x, index) => {
+                    return (
+                        <HiStar
+                            key={index}
+                            className={'w-5 h-5 text-gray-300'}
+                        />
+                    )
+                })}
             <p className={'pl-2 pb-1 text-base text-gray-500'}>
                 {person.length}
             </p>
         </div>
-    );
-};
+    )
+}
 
-export default RatesInClosetSpotlight;
+export default RatesInClosetSpotlight
