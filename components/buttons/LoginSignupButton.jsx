7ed03afac5071227c01ee80995602ad3761a06d8
Osini Kithma <66475844+swee76@users.noreply.github.com>
2022-07-28 22:25:37 +0530

    Style bug fixed in items add

--- components/buttons/LoginSignupButton.jsx
+++ components/buttons/LoginSignupButton.jsx
@@ -44,7 +44,7 @@ const LoginSignupButton = ({setLoggedIn, loggedIn}) => {
                     <button
                         onClick={logoutHandler}
                         type="button"
-                        className="inline-flex items-center px-4 py-2 border border-susty shadow-sm text-sm font-medium rounded-md text-red-500 bg-white hover:bg-red-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-susty"
+                        className="inline-flex items-center px-4 py-1.5 border border-susty shadow-sm text-sm font-medium rounded-md text-red-500 bg-white hover:bg-red-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-susty"
                     >
                         Logout
                     </button>
