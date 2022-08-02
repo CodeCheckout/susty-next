320584347dd3fb05afa1e27ad1a194ffcb5910e0
Nuwan Chamikara <39343048+gmnchamikara@users.noreply.github.com>
2022-08-02 17:33:27 +0530

    notifications

---
+++ components/Lists/Notification/notifications.jsx
@@ -0,0 +1,141 @@
+import React, {useEffect, useState} from 'react'
+
+
+const Notifications = () => {
+
+return (
+    <>
+      <div className="max-w-2xl text-gray-900 bg-white text-gray-700">
+        <div className="font-medium text-center text-xl"> Notifications</div>
+        <button
+          type="button"
+          className="relative inline-flex items-center w-full px-4 py-2 text-sm font-medium border-b"
+        >
+          <div className="grid grid-cols-2 gap-1 flex items-left">
+            <div className="mb-4">
+              <img
+                src="https://mdbootstrap.com//img/Photos/Square/1.jpg"
+                className="h-20 w-20 rounded-full"
+                alt=""
+              />
+            </div>
+          </div>
+
+          <div className=" text-sm font-medium text-gray-700 bg-white">
+            <a
+              href="#"
+              
+              className="block w-full px-1 py-2 "
+            >
+              Clothes you don't wear = extra cash. Sell them today. Upload now.
+            </a>
+            <a
+              href="#"
+              className="block w-full px-1 py-2 text-left"
+            >
+              16 hours ago
+            </a>
+          </div>
+
+        </button>
+        <button
+          type="button"
+          className="relative inline-flex items-center w-full px-4 py-2 text-sm font-medium border-b"
+        >
+          <div className="grid grid-cols-2 gap-1 flex items-left">
+            <div className="mb-4">
+              <img
+                src="https://mdbootstrap.com//img/Photos/Square/1.jpg"
+                className="h-20 w-20 rounded-full"
+                alt=""
+              />
+            </div>
+          </div>
+
+          <div className=" text-sm font-medium text-gray-700 bg-white">
+            <a
+              href="#"
+              
+              className="block w-full px-1 py-2 "
+            >
+              Clothes you don't wear = extra cash. Sell them today. Upload now.
+            </a>
+            <a
+              href="#"
+              className="block w-full px-1 py-2 text-left"
+            >
+              16 hours ago
+            </a>
+          </div>
+
+        </button>
+        <button
+          type="button"
+          className="relative inline-flex items-center w-full px-4 py-2 text-sm font-medium border-b"
+        >
+          <div className="grid grid-cols-2 gap-1 flex items-left">
+            <div className="mb-4">
+              <img
+                src="https://mdbootstrap.com//img/Photos/Square/1.jpg"
+                className="h-20 w-20 rounded-full"
+                alt=""
+              />
+            </div>
+          </div>
+
+          <div className=" text-sm font-medium text-gray-700 bg-white">
+            <a
+              href="#"
+              
+              className="block w-full px-1 py-2 "
+            >
+              Clothes you don't wear = extra cash. Sell them today. Upload now.
+            </a>
+            <a
+              href="#"
+              className="block w-full px-1 py-2 text-left"
+            >
+              16 hours ago
+            </a>
+          </div>
+
+        </button>
+        <button
+          type="button"
+          className="relative inline-flex items-center w-full px-4 py-2 text-sm font-medium border-b"
+        >
+          <div className="grid grid-cols-2 gap-1 flex items-left">
+            <div className="mb-4">
+              <img
+                src="https://mdbootstrap.com//img/Photos/Square/1.jpg"
+                className="h-20 w-20 rounded-full"
+                alt=""
+              />
+            </div>
+          </div>
+
+          <div className=" text-sm font-medium text-gray-700 bg-white">
+            <a
+              href="#"
+              
+              className="block w-full px-1 py-2 "
+            >
+              Clothes you don't wear = extra cash. Sell them today. Upload now.
+            </a>
+            <a
+              href="#"
+              className="block w-full px-1 py-2 text-left"
+            >
+              16 hours ago
+            </a>
+          </div>
+
+        </button>
+      </div>
+    </>
+)
+
+
+}
+
+export default Notifications
---
+++ pages/notifications/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Notification from '../../components/Lists/Notification/notifications'
+
+const Index = () => {
+    return (
+        <>
+            <Notification />
+        </>
+    )
+}
+
+export default Index
